c8e79dd94f4edb706b979e059a3c77acfca4c964
fdsa <gk06662611@edu.mon.bg>
2026-05-18 21:33:51 +0300

    fixed open class

--- src/Project/Commands/Basic/Open.java
+++ src/Project/Commands/Basic/Open.java
@@ -2,29 +2,50 @@ package Project.Commands.Basic;
 
 import Project.Commands.Context;
 import Project.Exceptions.NoElement;
+import Project.Exceptions.NoFile;
 import Project.Interfaces.Command;
-/**
- * Opens or references a file by name for subsequent operations.
- * Validates that a filename argument is provided.
- * Returns a confirmation message indicating the opened file.
- */
+import Project.Parts.Element;
+import java.io.File;
 
 public class Open implements Command {
+    
     @Override
-    public String execute(String[] args, Context context) throws NoElement
-    {
-        if (args.length < 1)
-        {
-            throw new NoElement("Too little arguments");
+    public String execute(String[] args, Context context) throws NoElement {
+        if (args == null || args.length < 1 || args[0] == null || args[0].isEmpty()) {
+            throw new NoElement("Too little arguments. Usage: open <filename>");
         }
-        String file = args[0];
+        
+        String filename = args[0];
 
-        return "Opened file" +file;
+        try {
+            File file = new File(filename);
+            Element rootElement;
+
+            if (file.exists()) {
+                rootElement = context.getSaveStorage().load(filename);
+                
+                if (rootElement == null) {
+                    rootElement = new Element("root");
+                    rootElement.id.put(0, 1);
+                }
+            } else {
+                rootElement = new Element("root");
+                rootElement.id.put(0, 1);
+                context.getSaveStorage().saving(rootElement, filename);
+            }
+
+            context.setFilename(filename);
+            context.setElement(rootElement);
+
+            return "Opened file " + filename;
+
+        } catch (Exception e) {
+            throw new NoFile("Error opening or creating file '" + filename + "': " + e.getMessage());
+        }
     }
 
     @Override
-    public String helpMsg()
-    {
+    public String helpMsg() {
         return "Opens file, if file doesnt exist, create new one";
     }
 }
